245ef20dd5e02b05fde9974c4bac77dd49c0e7e5
eminb <eminbaghmanov@gmail.com>
2022-12-13 22:53:40 -0500

    add2

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,5 +3,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("heeeeeeeeeeeeeeeeeeeeeeeeey");
         System.out.println("w--------------------------y");
+        System.out.println("firstBranch");
     }
 }
