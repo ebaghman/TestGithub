1d5e333ddc9ab60e419409d5485c0b43c4c8ca8d
eminb <eminbaghmanov@gmail.com>
2022-12-13 20:46:56 -0500

    added

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,5 +2,6 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("heeeeeeeeeeeeeeeeeeeeeeeeey");
+        System.out.println("w--------------------------y");
     }
 }
